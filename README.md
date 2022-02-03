394a604858150ee58994a261ae783b5229c04a40
Enrique Noriega <enrique.noriega@gmail.com>
2022-02-02 23:38:12 -0700

    Added negative examples to the dataset generator

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Fine tune a BERT checkpoint on biochemical relation extractions
 
 ## Scripts
 - `fries_to_bert.py`: Transforms a fries output from `REACH` into CONLL format for sequence tagging. This will be the tranining data for the model.
-- `mask_data_files.py`: Generates `N` copies of the unmasked data files following the masking parameters used by `RoBERTa`
+- `mask_data_files.py`: Generates `N` copies of the unmasked data files following the masking parameters used by `BERT`
+- `dataset_stats.py`: Prints statistics about the data.
--- fries_to_bert.py
+++ fries_to_bert.py
@@ -1,12 +1,14 @@
-from collections import defaultdict
-import json
 import itertools as it
-from os import error
-from pathlib import Path
-from tqdm import tqdm
-from typing import List, NamedTuple, Optional
-from pprint import pprint
+import json
 from collections import defaultdict
+from pathlib import Path
+from typing import List, NamedTuple, Optional, Tuple
+
+import typer as typer
+from tqdm import tqdm
+import numpy as np
+
+rng = np.random.RandomState(1024) # Set seed for reproducibility
 
 class EventFrame(NamedTuple):
 	args: List[str]
@@ -17,7 +19,7 @@ class EventFrame(NamedTuple):
 	passage_id: str
 	sentence_id: str
 
-def make_file_contents(pmcid:str, base_dir:Path = Path()) -> List[str]:
+def make_file_contents(pmcid:str, base_dir:Path = Path()) -> tuple[list[str], int, int]:
 	events_path = base_dir / f"{pmcid}.uaz.events.json"
 	sentences_path = base_dir / f"{pmcid}.uaz.sentences.json"
 	entities_path  = base_dir / f"{pmcid}.uaz.entities.json"
@@ -41,6 +43,7 @@ def make_file_contents(pmcid:str, base_dir:Path = Path()) -> List[str]:
 		simple_events, complex_events = {}, {}
 		successes, errors = 0, 0
 		for frame in json.load(f)['frames']:
+			# noinspection PyBroadException
 			try:
 				# Get the data from this frmae
 				trigger = frame['trigger'] if 'trigger' in frame else None
@@ -58,25 +61,25 @@ def make_file_contents(pmcid:str, base_dir:Path = Path()) -> List[str]:
 					simple_events[frame['frame-id']] = ef
 				
 				successes += 1
-			except Exception as ex:
+			except:
 				errors += 1
 
-	all_events = list(sorted(list(simple_events.values())+list(complex_events.values()), key=lambda ef:ef.sentence_id))
-	events_by_sent = it.groupby(all_events, key= lambda ef: ef.sentence_id)
+	all_events = list(sorted(list(simple_events.values()) + list(complex_events.values()), key=lambda e:e.sentence_id))
+	events_by_sent = it.groupby(all_events, key= lambda e: e.sentence_id)
 
 	blocks = list()
 
+	# Keep track of the sentences with event
+	sents_w_events = set()
+
 	for sent_id, events in events_by_sent:
-		sentence_frame = sentences[sent_id]
-		passage = sentences[sentence_frame['passage']]['text']
-		sent_start = int(sentence_frame['start-pos']['offset'])
-		sentence_text = passage[sentence_frame['start-pos']['offset']:sentence_frame['end-pos']['offset']]
-		tokens = sentence_text.split()
+		sents_w_events.add(sent_id) # We will use this to sample sentences w/o events
+		tokens, sent_start = get_sentence_tokens_and_starting_char(sent_id, sentences)
 
 		labels = set()
 		tags = defaultdict(set)
 
-		for ef in sorted(events, key= lambda ef: (ef.start, ef.end)):
+		for ef in sorted(events, key= lambda e: (e.start, e.end)):
 			labels.add(ef.type)
 			args = list(sorted((entities[a] for a in ef.args if a in entities), key=lambda a: a[0]))
 			index = 0
@@ -107,15 +110,43 @@ def make_file_contents(pmcid:str, base_dir:Path = Path()) -> List[str]:
 				fixed_tags.append(next(iter(l)))
 
 		#Build the block
-		blocks.append(' '.join(labels))
+		blocks.append('\t'.join(labels))
 		blocks.append('\n')
 		blocks.append('\t'.join(fixed_tags))
 		blocks.append('\n')
 		blocks.append('\t'.join(tokens))
 		blocks.append('\n')
 		blocks.append('\n')
-			
+
+	# For each block with events, find one w/o
+	sents_wo_events = list(set(sentences.keys()) - sents_w_events)
+
+	indices = rng.choice(sents_wo_events, min(len(sents_w_events), len(sents_wo_events)), replace=False)
+
+	for sent_id in indices:
+		tokens, _ = get_sentence_tokens_and_starting_char(sent_id, sentences)
+
+		# Build the block of the negative index
+		blocks.append('\t')
+		blocks.append('\n')
+		blocks.append('\t'.join(['O']*len(tokens)))
+		blocks.append('\n')
+		blocks.append('\t'.join(tokens))
+		blocks.append('\n')
+		blocks.append('\n')
+
 	return blocks, successes, errors
+
+
+def get_sentence_tokens_and_starting_char(sent_id: int, sentences) -> Tuple[List[str], int]:
+	""" Returns the tokens of sentence indexed by sent_id and the onset of the sentence in the frame """
+	sentence_frame = sentences[sent_id]
+	passage = sentences[sentence_frame['passage']]['text']
+	sent_start = sentence_frame['start-pos']['offset']
+	sentence_text = passage[sentence_frame['start-pos']['offset']:sentence_frame['end-pos']['offset']]
+	tokens = sentence_text.split()
+	return tokens, sent_start
+
 
 def process_dir(input_directory:Path, output_directory:Optional[Path] = Path()):
 	# Get the pmcids in the directory
